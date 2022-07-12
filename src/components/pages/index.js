aa777e3b8836dcd73a643d19e108d930e8a6426d
David DeCosta <DavidDeCosta12@gmail.com>
2022-07-12 18:14:32 -0400

    picture changes

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import '../pages/home.css';
+
+import homeBackround from '../pics/Arenal_Volcano.jpg';
   
 const Home = () => {
   return (
     <body id='homeContainer'>
+      <img src={homeBackround} alt="theBackround"/>
         <div className='insideHome'>
             There is No Place Like Home<br></br>
             But get out and Travel!
