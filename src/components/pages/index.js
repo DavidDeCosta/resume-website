1a5d0e64911ccfe7077d2d6110526e29f2787d96
David DeCosta <DavidDeCosta12@gmail.com>
2022-07-12 17:42:22 -0400

    home image change

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -4,7 +4,6 @@ import '../pages/home.css';
 const Home = () => {
   return (
     <body id='homeContainer'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
         <div className='insideHome'>
             There is No Place Like Home<br></br>
             But get out and Travel!
