50a6844dae701cec912b6ee833681e35d3810597
David DeCosta <DavidDeCosta12@gmail.com>
2022-07-13 22:48:18 -0400

    updated media resume viewing

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -8,7 +8,7 @@ const About=()=>{
             <video src='/videos/video-2.mp4' autoPlay loop muted />
         <div className='inside'>
             <i></i>
-            My name is David DeCosta and I want to make the world a better place for my Family!<br></br>
+            My name is David DeCosta and I want to make the world a better place for my Family.<br></br>
             Technology has alot of pro's and con's but the pro's far outweigh the cons.<br></br>
             Software has and will continue to be an amazing solution to many problems and I want to be part of the solution.<br></br>
             After years of working various job including being: A personal Trainer, Manager of a Mobile detailing company, CDL Truck driver,
