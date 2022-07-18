7cf7ef528a0509ddca6e51d078c1b11def1a84ab
David DeCosta <DavidDeCosta12@gmail.com>
2022-07-17 23:31:42 -0400

    changed job to jobs

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -11,7 +11,7 @@ const About=()=>{
             My name is David DeCosta and I want to make the world a better place for my family.<br></br>
             Technology has many pros and cons but the pros far outweigh the cons.<br></br>
             Software has and will continue to be an amazing solution to many problems and I want to be part of the solution.<br></br>
-            After years of working various job including being A personal Trainer, Manager of a Mobile detailing company,
+            After years of working various jobs including being A personal Trainer, Manager of a Mobile detailing company,
              CDL Truck driver, and working at several different restaurants I have found my passion.
         </div>
         </body>
