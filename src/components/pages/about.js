d1dd5c4f41c66d2f904a1115bb163afc727f96d5
David DeCosta <DavidDeCosta12@gmail.com>
2023-03-12 16:05:58 -0400

    changed the about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -8,10 +8,10 @@ const About=()=>{
             <video src='/videos/video-2.mp4' loop autoPlay muted playsinline="true" disablePictureInPicture="true" autoplay="autoplay" />
         <div className='inside'>
             <i></i>
-            My name is David DeCosta and I want to make the world a better place for my family.<br></br>
-            Software has and will continue to be an amazing solution to many problems and I want to be part of the solution.<br></br>
-            After years of working various jobs including being a personal Trainer, Manager of a Mobile detailing company,
-             CDL Truck driver, and working at several different restaurants I have found the path I want to be on.
+            My name is David DeCosta and my aspiration is to achieve positive change in society for the benefit of my family and the world at large.The software industry has <br />
+             demonstrated its potential as an effective means of addressing critical challenges, and I am eager to contribute to this cause. <br />
+            Drawing from my diverse work experience, which encompasses jobs as a personal trainer, a mobile detailing company manager, a CDL truck driver, and employment in <br />
+            several restaurants, I have determined that this is the path I wish to pursue.
         </div>
         </body>
     );
